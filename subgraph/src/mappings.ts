b41bff46b089b2b70cfdf352a4dd8aaf2d052390
Green <40367733+greenlucid@users.noreply.github.com>
2022-07-23 17:24:39 +0200

    feat(subgraph): most handlers

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,29 +1,35 @@
 import { CollectionCreated } from "../generated/HarbergerAdsFactory/HarbergerAdsFactory";
 import {
-  AdFundChanged,
   AdSet,
   Transfer,
-  ValuationChanged,
+  ValuationSet,
   TaxPaid,
+  TokenFunded,
+  HarbergerAds
 } from "../generated/templates/HarbergerAds/HarbergerAds";
 import { Ad, Collection } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handleCollectionCreated(event: CollectionCreated): void {
-  let collection = new Collection(event.params.contractAddress.toHex());
-  collection.adCount = event.params.adCount;
-  collection.collector = event.params.collector;
-  collection.cooldownPeriod = event.params.cooldownPeriod;
-  collection.currency = event.params.currency;
-  collection.name = event.params.name;
-  collection.taxRate = event.params.taxRate;
+  let collection = new Collection(event.params._address.toHexString());
+  collection.adCount = event.params._adCount;
+  collection.collector = event.params._collector;
+  collection.cooldownPeriod = event.params._cooldownPeriod;
+  collection.currency = event.params._currency;
+  collection.taxRate = event.params._taxRate;
+
+  let collectionContract = HarbergerAds.bind(event.params._address);
+  collection.name = collectionContract.name();
+  collection.symbol = collectionContract.symbol();
+  collection.tokenURI = collectionContract.tokenURI(BigInt.fromU32(0));
   collection.save();
 }
 
 export function handleTransfer(event: Transfer): void {
-  let ad = Ad.load(event.params.tokenId);
+  let ad = Ad.load(`${event.params.tokenId}@${event.address}`);
 
   if (!ad) {
-    ad = new Ad(event.params.tokenId);
+    ad = new Ad(`${event.params.tokenId}@${event.address}`);
   }
 
   ad.owner = event.params.to;
@@ -31,26 +37,29 @@ export function handleTransfer(event: Transfer): void {
 }
 
 export function handleAdSet(event: AdSet): void {
-  let ad = Ad.load(event.params.tokenId);
+  let ad = Ad.load(`${event.params.tokenId}@${event.address}`) as Ad;
   ad.uri = event.params.uri;
   ad.save();
 }
 
-export function handleAdFundChanged(event: AdFundChanged): void {
-  let ad = Ad.load(event.params.tokenId);
-  ad.fund = event.params.value;
+export function handleTokenFunded(event: TokenFunded): void {
+  let ad = Ad.load(`${event.params._tokenId}@${event.address}`) as Ad;
+  ad.fund = event.params._amount;
   ad.save();
 }
 
-export function handleValuationChanged(event: ValuationChanged): void {
-  let ad = Ad.load(event.params.tokenId);
-  ad.valuation = event.params.valuation;
-  // ad.nextValuationTimestamp
+export function handleValuationChanged(event: ValuationSet): void {
+  let ad = Ad.load(`${event.params._tokenId}@${event.address}`) as Ad;
+  ad.valuation = event.params._valuation;
+  ad.nextValuationTimestamp = event.block.timestamp;
+
   ad.save();
 }
 
 export function handleTaxPaid(event: TaxPaid): void {
-  let ad = Ad.load(event.params.tokenId);
-  // ad.lastPaidTimestamp
+  let ad = Ad.load(`${event.params._tokenId}@${event.address}`) as Ad;
+  ad.lastPaidTimestamp = event.block.timestamp;
+  ad.fund = ad.fund.minus(event.params._value);
+  
   ad.save();
 }
