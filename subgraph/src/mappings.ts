4c5a43d2b638df7ab49afc313c832cb8e4061c43
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-07-23 13:50:41 +0300

    subgraph yaml

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,33 +1,30 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  HarbergerAdsFactory,
-  HarbergerAdsCreated
-} from "../generated/HarbergerAdsFactory/HarbergerAdsFactory"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { HarbergerAdsFactory, HarbergerAdsCreated } from "../generated/HarbergerAdsFactory/HarbergerAdsFactory";
+import { ExampleEntity } from "../generated/schema";
 
 export function handleHarbergerAdsCreated(event: HarbergerAdsCreated): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = ExampleEntity.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new ExampleEntity(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity._address = event.params._address
-  entity._adCount = event.params._adCount
+  entity._address = event.params._address;
+  entity._adCount = event.params._adCount;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
