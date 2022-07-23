00d7762c697d501e3768b28331284c97259ed0df
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-07-23 12:48:32 +0300

    emit name on creation from factory

--- contracts/src/HarbergerAdsFactory.sol
+++ contracts/src/HarbergerAdsFactory.sol
@@ -5,8 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./HarbergerAds.sol";
 
 contract HarbergerAdsFactory {
-  
+
   event HarbergerAdsCreated(
+    string indexed _name,
     HarbergerAds _address,
     uint256 _adCount,
     uint256 _taxRate,
@@ -15,8 +16,8 @@ contract HarbergerAdsFactory {
     address _collector
   );
 
-  function create(uint256 _adCount, uint256 _taxRate, uint256 _cooldownPeriod, IERC20 _currency, address _collector) public {
+  function create(uint256 _adCount, uint256 _taxRate, uint256 _cooldownPeriod, IERC20 _currency, address _collector, string calldata _name) public {
     HarbergerAds harbergerAdsContract = new HarbergerAds(_adCount, _taxRate, _cooldownPeriod, _currency, _collector);
-    emit HarbergerAdsCreated(harbergerAdsContract, _adCount, _taxRate, _cooldownPeriod, _currency, _collector);
+    emit HarbergerAdsCreated(_name, harbergerAdsContract, _adCount, _taxRate, _cooldownPeriod, _currency, _collector);
   }
 }
